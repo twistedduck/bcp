cb45f8dd62e5d098dc4be4e5195b4736b0d1a49f
Seth Hayward <seth@twistedduck.com>
2021-06-27 16:41:04 +0100

    Big-step reduction (ex 4.2.2)

--- c4/core.ml
+++ c4/core.ml
@@ -46,7 +46,37 @@ let rec eval1 t = match t with
   | _ ->
       raise NoRuleApplies
 
+let rec eval2 t =
+  match t with
+  | v when (isval v) -> v
+  | TmIf(_,t1,t2,t3) ->
+      ( let t1' = eval2 t1 in
+      match t1' with
+      | TmTrue(_) -> t2
+      | TmFalse(_) -> t3
+      | _ -> raise NoRuleApplies )
+  | TmSucc(fi, t1) ->
+      let t1' = eval2 t1 in
+      if isnumericval t1' then
+        TmSucc(fi, t1')
+      else
+        raise NoRuleApplies
+  | TmPred(fi, t1) ->
+      ( let t1' = eval2 t1 in
+      match t1' with
+      | TmZero(_) -> TmZero(dummyinfo)
+      | TmSucc(_, nv1) when (isnumericval nv1) ->
+          nv1
+      | _ -> raise NoRuleApplies )
+  | TmIsZero(fi, t1) ->
+      ( let t1' = eval2 t1 in
+      match t1' with
+      | TmZero(_) -> TmTrue(dummyinfo)
+      | TmSucc(_, nv1) when (isnumericval nv1) -> TmFalse(dummyinfo)
+      | _ -> raise NoRuleApplies )
+  | _ -> raise NoRuleApplies
+
 let rec eval t =
-  try let t' = eval1 t
-      in eval t'
+  try let t' = eval2 t
+      in if isval t' then t' else eval t'
   with NoRuleApplies -> t
